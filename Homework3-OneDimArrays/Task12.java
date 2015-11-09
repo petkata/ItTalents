9972d6873a9222db9bc62fb69782f8e1fcf02a5d
petkata <petyr_yontchev@abv.bg>
2015-11-09 18:24:01 +0200

    Task 12 complete

--- Homework3-OneDimArrays/Task12.java
+++ Homework3-OneDimArrays/Task12.java
@@ -14,6 +14,41 @@ public class Task12 {
 //		- 2 i 3 chrez sabirane;
 //		- 4 i 5 chrez umnozhenie.
 
+		int[] arr = {2,3,4,-5,6,-7,8};
 		
+		// Printing the original array
+		System.out.print("[");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]);
+			if (i < arr.length -1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println("]");
+		
+		//Swapping [0] and [1]
+		int temp = arr[0] ;
+		arr[0] = arr[1];
+		arr[1] = temp;
+		
+		//swapping [2] and [3]
+		arr[2] = arr[2] + arr[3];
+		arr[3] = arr[2] - arr[3];
+		arr[2] = arr[2] - arr[3];
+		
+		//swapping [4] and [5]
+		arr[4] = arr[4] * arr[5];
+		arr[5] = arr[4] / arr[5];
+		arr[4] = arr[4] / arr[5];
+		
+		//After changes
+		System.out.print("[");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]);
+			if (i < arr.length -1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println("]");
 	}
 }
